480390c7ec4298b603bed18a8282210ccec1c8c6
BitmecDevelop <git>
2021-06-22 20:24:52 -0600

    first commit

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -18,10 +18,31 @@ class Home extends StatelessWidget {
                 [
                   Container(
                     height: 500.0,
-                    margin: EdgeInsets.symmetric(vertical: 15.0,),
+                    margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
                     child: Column(
                       children: [
-                         
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)
+                          ),
+                          elevation: 3.0,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 15.0),
+                            child: ListTile(
+                              title: Text('Citas'),
+                              subtitle: Text('Agendar citas'),
+                              trailing: Icon(Icons.chevron_right, size: 40.0,),
+                              leading: Container(
+                                padding: EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Color(0XFFE2E9FB)
+                                ),
+                                child: Image.asset('assets/images/medical.png')
+                              ),
+                            ),
+                          ),
+                        )
                       ],
                     )
                   )
