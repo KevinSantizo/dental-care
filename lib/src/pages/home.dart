9bf79078e9cb003c132000cef7242eb475ae4bfd
BitmecDevelop <git>
2021-06-24 12:57:37 -0600

    commit

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -9,49 +9,90 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: CustomScrollView(
-          slivers: [
-            AppBarComponent(),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  Container(
-                    height: 500.0,
-                    margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-                    child: Column(
-                      children: [
-                        Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0)
-                          ),
-                          elevation: 3.0,
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 15.0),
-                            child: ListTile(
-                              title: Text('Citas'),
-                              subtitle: Text('Agendar citas'),
-                              trailing: Icon(Icons.chevron_right, size: 40.0,),
-                              leading: Container(
-                                padding: EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  color: Color(0XFFE2E9FB)
+      backgroundColor: Color(0xffF4FCF9),
+      body: Stack(
+        children: [
+          Container(
+            child: CustomScrollView(
+              slivers: [
+                AppBarComponent(),
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                        child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0)
+                            ),
+                            elevation: 3.0,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 15.0),
+                              child: ListTile(
+                                title: Text('Citas'),
+                                subtitle: Text('Agendar citas'),
+                                trailing: Icon(Icons.chevron_right, size: 40.0,),
+                                leading: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Color(0XFFE2E9FB)
+                                  ),
+                                  child: Image.asset('assets/images/medical.png')
                                 ),
-                                child: Image.asset('assets/images/medical.png')
+                              ),
+                            ),
+                          ),
+                      ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0, ),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0)
+                            ),
+                            elevation: 3.0,
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 15.0),
+                              child: ListTile(
+                                title: Text('Tratamientos'),
+                                subtitle: Text('Ver tratamientos'),
+                                trailing: Icon(Icons.chevron_right, size: 40.0,),
+                                leading: Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    color: Color(0XFFE2E9FB)
+                                  ),
+                                  child: Image.asset('assets/images/medical.png')
+                                ),
                               ),
                             ),
                           ),
                         )
-                      ],
-                    )
+                    ]
                   )
-                ]
-              )
+                )
+              ],
             )
-          ],
-        )
-      )
+          ),
+          DraggableScrollableSheet(
+            minChildSize: 0.45,
+            initialChildSize: 0.45,
+            maxChildSize: 0.45,
+            builder: (context, scrollController) => Material(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(40.0)),
+              color: Colors.white,
+              elevation: 10,
+              child: ListView.builder(
+                controller: scrollController,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text("Item $index"),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
